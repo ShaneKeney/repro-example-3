b5f6b5de71b014df95bb8cd2920cfd6caf813fb1
Shane Keney <sckeney@gmail.com>
2024-07-03 16:09:26 -0400

    chore: adjust build values

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,11 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 
 export const ENV = {
-  EXPO_PUBLIC_APP_DISPLAY_NAME: '(Dev) Reliant',
-  EXPO_PUBLIC_PACKAGE: 'com.reliant.mobile.dev',
+  EXPO_PUBLIC_APP_DISPLAY_NAME: '(Dev) Repro3',
+  EXPO_PUBLIC_PACKAGE: 'com.repro3.mobile.dev',
   EXPO_PUBLIC_FOREGROUND_IMAGE: './assets/adaptive-icon.png',
-  EXPO_PUBLIC_SCHEME: 'reliantdev',
-  EXPO_PUBLIC_BUNDLE_ID: 'com.reliant.mobile.dev',
+  EXPO_PUBLIC_SCHEME: 'repro3dev',
+  EXPO_PUBLIC_BUNDLE_ID: 'com.repro3.mobile.dev',
   EXPO_PUBLIC_ICON_PATH: './assets/dev/icon.dev.png',
   EXPO_PUBLIC_APP_VERSION: '2.2',
   EXPO_PUBLIC_ANDROID_VERSION_CODE: '2000002',
@@ -28,9 +28,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // Spreads static configuration from app.json if we want to seperate out and be more
   // explicit about what should / and should not change
   ...config,
-  name: ENV.EXPO_PUBLIC_APP_DISPLAY_NAME || '(Dev) Reliant',
-  owner: 'reliant-capital',
-  slug: 'reliant',
+  name: ENV.EXPO_PUBLIC_APP_DISPLAY_NAME || '(Dev) Repro3',
+  owner: 'rally2',
+  slug: 'repro3',
   description:
     'Expo Router & Nativewind Template to quick project spin up.  Configured environments, EAS profiles & more!',
   privacy: 'hidden',
@@ -46,8 +46,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     backgroundColor: '#FFFFFF'
   },
   updates: {
-    fallbackToCacheTimeout: 1000,
-    url: 'https://u.expo.dev/036c74e4-180c-4ac3-824c-2d27092aafd2'
+    fallbackToCacheTimeout: 1000
   },
   runtimeVersion: {
     policy: 'appVersion'
@@ -74,7 +73,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     ...ENV,
     eas: {
-      projectId: '036c74e4-180c-4ac3-824c-2d27092aafd2'
+      projectId: '9f728e98-9311-429f-a5d7-97b8d317849c'
     }
   },
   experiments: {
@@ -86,11 +85,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 
 // TODO: Potentially remove having leveraged .env files and eas.json instead
 function setupProductionEnvironment() {
-  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = 'Reliant';
-  ENV.EXPO_PUBLIC_PACKAGE = 'com.reliant.mobile';
+  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = 'Repro3';
+  ENV.EXPO_PUBLIC_PACKAGE = 'com.repro3.mobile';
   ENV.EXPO_PUBLIC_FOREGROUND_IMAGE = './assets/adaptive-icon.png';
-  ENV.EXPO_PUBLIC_SCHEME = 'reliant';
-  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.reliantcapitalgrp.rcfinancecalculator';
+  ENV.EXPO_PUBLIC_SCHEME = 'repro3';
+  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.repro3.mobile';
   ENV.EXPO_PUBLIC_ICON_PATH = './assets/icon.png';
   ENV.EXPO_PUBLIC_APP_VERSION = '2.2';
   ENV.EXPO_PUBLIC_ANDROID_VERSION_CODE = '2000002';
@@ -101,11 +100,11 @@ function setupProductionEnvironment() {
 
 // TODO: Potentially remove having leveraged .env files and eas.json instead
 function setupStagingEnvironment() {
-  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = '(UAT) Reliant';
-  ENV.EXPO_PUBLIC_PACKAGE = 'com.reliant.mobile.uat';
+  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = '(UAT) Repro3';
+  ENV.EXPO_PUBLIC_PACKAGE = 'com.repro3.mobile.uat';
   ENV.EXPO_PUBLIC_FOREGROUND_IMAGE = './assets/adaptive-icon.png';
-  ENV.EXPO_PUBLIC_SCHEME = 'reliantuat';
-  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.reliant.mobile.uat';
+  ENV.EXPO_PUBLIC_SCHEME = 'repro3uat';
+  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.repro3.mobile.uat';
   ENV.EXPO_PUBLIC_ICON_PATH = './assets/staging/icon.staging.png';
   ENV.EXPO_PUBLIC_APP_VERSION = '2.2';
   ENV.EXPO_PUBLIC_ANDROID_VERSION_CODE = '2000002';
